8313044d9b95753c07f4486e8dc0cbdaa2203266
Sol-Ahn <sorry0194@gmail.com>
2023-07-25 23:08:07 +0900

    feat: Card 컴포넌트를 분리한다

--- src/components/common/CustomContainer.jsx
+++ src/components/common/CustomContainer.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import CustomCard from './CustomCard';
 import items from '../../data.json';
 
 const CustomContainer = (props) => {
@@ -50,31 +48,7 @@ const CustomContainer = (props) => {
 				<Grid container spacing={4}>
 					{items.map((item) => (
 						<Grid item key={item.id} xs={12} sm={6} md={4}>
-							<Card
-								sx={{
-									height: '100%',
-									display: 'flex',
-									flexDirection: 'column',
-								}}
-							>
-								<CardMedia
-									component='div'
-									sx={{
-										// 16:9
-										pt: '56.25%',
-									}}
-									image='https://source.unsplash.com/random?wallpapers'
-								/>
-								<CardContent sx={{ flexGrow: 1 }}>
-									<Typography gutterBottom variant='h5' component='h2'>
-										Heading
-									</Typography>
-									<Typography>
-										This is a media card. You can use this section to describe
-										the content.
-									</Typography>
-								</CardContent>
-							</Card>
+							<CustomCard item={item} />
 						</Grid>
 					))}
 				</Grid>
